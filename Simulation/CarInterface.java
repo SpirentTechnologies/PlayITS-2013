8077a2e68f8f3343d531ea9bfe4300daaa93fa4a
kurosch <p.kurosch@googlemail.com>
2013-02-22 15:23:47 +0100

    Update Simulation/CarInterface.java

--- Simulation/CarInterface.java
+++ Simulation/CarInterface.java
@@ -6,8 +6,6 @@ public interface CarInterface {
 	double setSpeed(double speed); //set speed of car
 	double getSpeed(); // get speed of car
 	boolean toggleESP(); // esp on off
-	boolean toggleABS(); // abs on off
-	boolean toggleAirbag(); //airbag on off
 	boolean toggleWarningSender(); // send warnings on off -> warnings between cars
 	boolean toggleWarningReceiver(); // receive warnings on off -> warnings between cars
 	boolean toggleLightSensor(); //lightsensor on off
@@ -15,10 +13,7 @@ public interface CarInterface {
 	GPSposition getGPSPosition(); // get gpsposition
 	GPSposition setGPSPosition(GPSposition position); // set gpsposition
 	double getTankFill(); // tank fill sensor -> get its tank fill
-	boolean triggerAirbag(); // airbag pressure trigger
-	void breaking(); // breaking length stuff
-	Damage getDamage(); // get car damage
-	Damage setDamage(Damage d); // set car damage
+	double breaking(); // breaking length stuff
 	void update(); // update all resources 
 	
 }
