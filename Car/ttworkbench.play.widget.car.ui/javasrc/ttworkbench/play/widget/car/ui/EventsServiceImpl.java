5686f821d99a3768280cb8bb53d428ab8e067d0e
kensan <bgoverlord@gmail.com>
2013-03-01 23:56:49 +0100

    Comment removed

--- Car/ttworkbench.play.widget.car.ui/javasrc/ttworkbench/play/widget/car/ui/EventsServiceImpl.java
+++ Car/ttworkbench.play.widget.car.ui/javasrc/ttworkbench/play/widget/car/ui/EventsServiceImpl.java
@@ -1,41 +1,4 @@
-/*
- * ----------------------------------------------------------------------------
- *  (C) Copyright Testing Technologies, 2012.  All Rights Reserved.
- *
- *  All copies of this program, whether in whole or in part, and whether
- *  modified or not, must display this and all other embedded copyright
- *  and ownership notices in full.
- *
- *  See the file COPYRIGHT for details of redistribution and use.
- *
- *  You should have received a copy of the COPYRIGHT file along with
- *  this file; if not, write to the Testing Technologies,
- *  Michaelkirchstr. 17/18, 10179 Berlin, Germany.
- *
- *  TESTING TECHNOLOGIES DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
- *  SOFTWARE. IN NO EVENT SHALL TESTING TECHNOLOGIES BE LIABLE FOR ANY
- *  SPECIAL, DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
- *  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
- *  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
- *  THIS SOFTWARE.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
- *  EITHER EXPRESSED OR IMPLIED, INCLUDING ANY KIND OF IMPLIED OR
- *  EXPRESSED WARRANTY OF NON-INFRINGEMENT OR THE IMPLIED WARRANTIES
- *  OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
- *
- * -----------------------------------------------------------------------------
- *
- *  AUTHOR:    Bogdan Stanca
- *  DATE:      Feb 6, 2013
- *
- *  REVISION INFO:
- *    $Revision: 1.5 $ $Date: 2013/02/12 12:57:34 $
- *    $Source: /usr/local/cvs_root/TTplugins/Play/widgets/com.testingtech.ttworkbench.play.widget.pop3.ui/javasrc/com/testingtech/ttworkbench/play/widget/pop3/ui/EventsServiceImpl.java,v $
- * 
- * -----------------------------------------------------------------------------
- */
+
 package ttworkbench.play.widget.car.ui;
 
 import ttworkbench.play.widget.car.ui.model.CarStatusModel;
