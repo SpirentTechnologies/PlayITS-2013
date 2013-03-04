119aeda794a27962402e5dc787ad4355a271cb7d
Kurosch <kurosch@crunchbang>
2013-03-04 11:54:52 +0100

    connection init

--- Simulation/ttworkbench.play.simulation.car/src/com/testingtech/ttworkbench/play/simulation/car/Socket.java
+++ Simulation/ttworkbench.play.simulation.car/src/com/testingtech/ttworkbench/play/simulation/car/Socket.java
@@ -1,14 +1,21 @@
 package com.testingtech.ttworkbench.play.simulation.car;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 
-import com.google.protobuf.RpcCallback;
-import com.google.protobuf.RpcChannel;
+
+import java.util.concurrent.Executors;
+
+import com.google.protobuf.BlockingRpcChannel;
 import com.google.protobuf.RpcController;
 import com.googlecode.protobuf.socketrpc.RpcChannels;
+import com.googlecode.protobuf.socketrpc.RpcConnectionFactory;
+import com.googlecode.protobuf.socketrpc.RpcServer;
+import com.googlecode.protobuf.socketrpc.ServerRpcConnectionFactory;
 import com.googlecode.protobuf.socketrpc.SocketRpcConnectionFactories;
 import com.googlecode.protobuf.socketrpc.SocketRpcController;
+import com.testingtech.ttworkbench.play.generated.PROTO_SUTAPI;
+
+
+
 
 public class Socket {
 
@@ -20,23 +27,40 @@ public class Socket {
 
 	}
 
-	public void test() {
 
+	void test(){
+		int port = 13333;
+		startActionService(port);
+		createEventsClient();
+	}
+
+
+	private void createEventsClient(int port, String host) {
 		// Create channel
 		RpcConnectionFactory connectionFactory = SocketRpcConnectionFactories
-				.createRpcConnectionFactory(host, port);
-		BlockingRpcChannel channel = RpcChannels
-				.newBlockingRpcChannel(connectionFactory);
+		    .createRpcConnectionFactory(host, port);
+		BlockingRpcChannel channel = RpcChannels.newBlockingRpcChannel(connectionFactory);
 
 		// Call service
-		BlockingInterface service = MyService.newBlockingStub(channel);
-		RpcController controller = new SocketRpcController();
-		MyResponse myResponse = service.myMethod(controller, myRequest);
+		PROTO_SUTAPI.EVENTS.BlockingInterface service = PROTO_SUTAPI.EVENTS.newBlockingStub(channel);
+		RpcController rpcController = new SocketRpcController();
+		MyResponse myResponse = service.sendStatus(rpcController, myRequest);
 
 		// Check success
 		if (rpcController.failed()) {
-			System.err.println(String.format("Rpc failed %s : %s",
-					rpcController.errorReason(), rpcController.errorText()));
-		}
+		  System.err.println(String.format("Rpc failed %s",
+		      rpcController.errorText()));
+		}		
+	}
+
+
+	private void startActionService(int port) {
+		// Start server
+		ServerRpcConnectionFactory rpcConnectionFactory = SocketRpcConnectionFactories.createServerRpcConnectionFactory(port);
+		RpcServer server = new RpcServer(rpcConnectionFactory, 
+		    Executors.newFixedThreadPool(10), true);
+		server.registerBlockingService(PROTO_SUTAPI.ACTIONS.newReflectiveBlockingService(new ActionsServiceImpl())); // For blocking impl
+		server.run();
 	}
 }
+
