eaae3f8b31bd60355fd388c9c3ba7c4317fae7c5
Bogdan Stanca-Kaposta <stanca@testingtech.com>
2013-03-15 10:54:08 +0100

    empty implemented all functions

--- Simulation/ttworkbench.play.simulation.car/src/com/testingtech/ttworkbench/play/simulation/car/ActionsServiceImpl.java
+++ Simulation/ttworkbench.play.simulation.car/src/com/testingtech/ttworkbench/play/simulation/car/ActionsServiceImpl.java
@@ -2,13 +2,14 @@ package com.testingtech.ttworkbench.play.simulation.car;
 
 import com.google.protobuf.RpcController;
 import com.google.protobuf.ServiceException;
-import com.testingtech.ttworkbench.play.generated.PROTO_API.carStatusType;
-import com.testingtech.ttworkbench.play.generated.PROTO_API.Void;
 import com.testingtech.ttworkbench.play.generated.PROTO_API.ACTIONS.BlockingInterface;
+import com.testingtech.ttworkbench.play.generated.PROTO_API.Void;
+import com.testingtech.ttworkbench.play.generated.PROTO_API.carInitType;
 import com.testingtech.ttworkbench.play.generated.PROTO_API.onOffEngineType;
 import com.testingtech.ttworkbench.play.generated.PROTO_API.speedType;
 import com.testingtech.ttworkbench.play.generated.PROTO_API.trackType;
 import com.testingtech.ttworkbench.play.generated.PROTO_API.warningType;
+import com.testingtech.ttworkbench.play.generated.PROTO_API.widgetExit;
 
 public class ActionsServiceImpl implements BlockingInterface {
 	Car car;
@@ -21,21 +22,21 @@ public class ActionsServiceImpl implements BlockingInterface {
 	public Void aPIOnOffEngineType(RpcController controller,
 			onOffEngineType request) throws ServiceException {
 		car.engine = request.getEngineStatus();
-		return null;
+		return nil();
 	}
 
 	@Override
 	public Void aPISpeedType(RpcController controller, speedType request)
 			throws ServiceException {
 		car.speed = request.getSpeed();
-		return null;
+		return nil();
 	}
 
 	@Override
 	public Void aPITrackType(RpcController controller, trackType request)
 			throws ServiceException {
 		// TODO get track type from car
-		return null;
+		return nil();
 	}
 
 	@Override
@@ -44,7 +45,25 @@ public class ActionsServiceImpl implements BlockingInterface {
 		Warnings danger = Warnings.valueOf(request.getWarningName().getEnumValue().toString());
 		Tupel<Warnings, GPSposition> warning = new Tupel<Warnings, GPSposition>(danger, new GPSposition(0, 0));
 		car.addWarning(warning);
-		
-		return null;
+
+		return nil();
+	}
+
+	@Override
+	public Void aPICarInitType(RpcController controller, carInitType request)
+			throws ServiceException {
+		// TODO Auto-generated method stub
+		return nil();
+	}
+
+	@Override
+	public Void aPIWidgetExit(RpcController controller, widgetExit request)
+			throws ServiceException {
+		// TODO Auto-generated method stub
+		return nil();
+	}
+
+	protected Void nil() {
+		return Void.newBuilder().build();
 	}
 }
