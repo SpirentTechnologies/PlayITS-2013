e5fc98da2c0dbef1706a4299d9f4f8c44f2b2a18
kensan <bgoverlord@gmail.com>
2013-03-24 08:49:29 +0100

    update View

--- Car/ttworkbench.play.widget.car.ui/javasrc/ttworkbench/play/widget/car/ui/CarWidgetFrame.java
+++ Car/ttworkbench.play.widget.car.ui/javasrc/ttworkbench/play/widget/car/ui/CarWidgetFrame.java
@@ -14,6 +14,11 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Slider;
 
 import ttworkbench.play.widget.car.ui.model.CarStatusModel;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class CarWidgetFrame extends Composite implements IUIController{
 
@@ -25,6 +30,7 @@ public class CarWidgetFrame extends Composite implements IUIController{
 	private Color red = new Color(Display.getCurrent(),255,0,0);
 	private Color green = new Color(Display.getCurrent(),0,255,0);
 	private Browser browser;
+	private Label lblSpeed;
 	/**
 	 * Create the composite.
 	 * @param parent
@@ -32,7 +38,7 @@ public class CarWidgetFrame extends Composite implements IUIController{
 	 */
 	public CarWidgetFrame(Composite parent, int style, ICommunication comm) {
 		super(parent, style);
-		setLayout(new GridLayout(11, false));
+		setLayout(new GridLayout(4, false));
 		status = comm.getCarModel().getStatus();
 		controller = comm.getWidgetController();
 		Label lblAbs = new Label(this, SWT.CENTER);
@@ -55,18 +61,10 @@ public class CarWidgetFrame extends Composite implements IUIController{
 		
 		browser = new Browser(this, SWT.NONE);
 		browser.setUrl("E:\\Users\\kensan.Phenom-PC\\git\\PlayITS\\Car\\ttworkbench.play.widget.car.ui\\www\\car.html");
-		GridData gd_browser = new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 6);
-		gd_browser.heightHint = 372;
-		gd_browser.widthHint = 362;
+		GridData gd_browser = new GridData(SWT.CENTER, SWT.CENTER, false, false, 2, 11);
+		gd_browser.heightHint = 230;
+		gd_browser.widthHint = 284;
 		browser.setLayoutData(gd_browser);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
 		
 		Label lblEsp = new Label(this, SWT.CENTER);
 		GridData gd_lblEsp = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
@@ -92,15 +90,6 @@ public class CarWidgetFrame extends Composite implements IUIController{
 			lblLight.setBackground(red);
 		}
 		
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		
 		btnEngine = new Button(this, SWT.NONE);
 		btnEngine.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -116,18 +105,10 @@ public class CarWidgetFrame extends Composite implements IUIController{
 		btnEngine.setText("Engine");
 		
 		lblOnOff = new Label(this, SWT.NONE);
-		lblOnOff.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblOnOff.setAlignment(SWT.CENTER);
+		lblOnOff.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 		lblOnOff.setText("Off");
 		lblOnOff.setBackground(red);
-		
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
 		
 		Label lblNewLabel = new Label(this, SWT.CENTER);
 		GridData gd_lblNewLabel = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
@@ -135,22 +116,53 @@ public class CarWidgetFrame extends Composite implements IUIController{
 		lblNewLabel.setLayoutData(gd_lblNewLabel);
 		lblNewLabel.setText("Speed");
 		
-		Label lblSpeed = new Label(this, SWT.NONE);
-		lblSpeed.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblSpeed = new Label(this, SWT.NONE);
+		lblSpeed.setAlignment(SWT.CENTER);
+		lblSpeed.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 		lblSpeed.setText("0");
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
-		new Label(this, SWT.NONE);
 		
 		sliderSpeed = new Slider(this, SWT.NONE);
+		sliderSpeed.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+				lblSpeed.setText(""+sliderSpeed.getSelection() * status.getMaxSpeed()/100);
+				lblSpeed.setBackground(red);
+			}
+		});
+
 		GridData gd_sliderSpeed = new GridData(SWT.FILL, SWT.CENTER, false, false, 2, 1);
 		gd_sliderSpeed.widthHint = 106;
 		sliderSpeed.setLayoutData(gd_sliderSpeed);
+		
+		Label lblFuellevel = new Label(this, SWT.NONE);
+		lblFuellevel.setText("fuellevel");
+		
+		ProgressBar progressBarfuel = new ProgressBar(this, SWT.NONE);
+		GridData gd_progressBarfuel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_progressBarfuel.widthHint = 74;
+		progressBarfuel.setLayoutData(gd_progressBarfuel);
+		
+		Label lblConsumption = new Label(this, SWT.NONE);
+		lblConsumption.setText("consumption");
+		
+		Label consumptionMeter = new Label(this, SWT.NONE);
+		consumptionMeter.setAlignment(SWT.CENTER);
+		consumptionMeter.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		consumptionMeter.setText("0");
+		
+		Button btnIce = new Button(this, SWT.NONE);
+		btnIce.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		btnIce.setText("Ice");
+		
+		Button btnNewButton_1 = new Button(this, SWT.NONE);
+		btnNewButton_1.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		btnNewButton_1.setText("DEER");
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
 
 	}
 
--- Car/ttworkbench.play.widget.car.ui/javasrc/ttworkbench/play/widget/car/ui/InitializeFrame.java
+++ Car/ttworkbench.play.widget.car.ui/javasrc/ttworkbench/play/widget/car/ui/InitializeFrame.java
@@ -1,7 +1,5 @@
 package ttworkbench.play.widget.car.ui;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.File;
 
 import javax.swing.JFileChooser;
@@ -10,6 +8,8 @@ import javax.swing.JOptionPane;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -39,90 +39,61 @@ public class InitializeFrame extends Composite{
 		setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_LIGHT_SHADOW));
 		me = this;
 		controller = comm.getWidgetController();
-		setLayout(null);
-		
-		Button btnInitialize = new Button(this, SWT.NONE);
-		btnInitialize.setBounds(176, 254, 75, 25);
-		btnInitialize.setText("initialize");
-		
-		fuel = new Text(this, SWT.BORDER | SWT.CENTER);
-		fuel.setBounds(280, 61, 76, 21);
-		
-		Label lblFuel = new Label(this, SWT.CENTER);
-		lblFuel.setBounds(190, 67, 75, 15);
-		lblFuel.setAlignment(SWT.CENTER);
-		lblFuel.setText("fuel");
-		
-		Label lblSpeed = new Label(this, SWT.CENTER);
-		lblSpeed.setBounds(190, 104, 81, 15);
-		lblSpeed.setText("max. speed");
-		lblSpeed.setAlignment(SWT.CENTER);
-		
-		maxspeed = new Text(this, SWT.BORDER | SWT.CENTER);
-		maxspeed.setBounds(280, 104, 76, 21);
-		
-		btnAbsSensor = new Button(this, SWT.CHECK | SWT.CENTER);
-		btnAbsSensor.setBounds(70, 67, 93, 16);
-		btnAbsSensor.setText("ABS Sensor");
-		
-		btnEspSensor = new Button(this, SWT.CHECK | SWT.CENTER);
-		btnEspSensor.setBounds(70, 109, 93, 16);
-		btnEspSensor.setText("ESP Sensor");
-		
-		btnFoglightSensor = new Button(this, SWT.CHECK | SWT.CENTER);
-		btnFoglightSensor.setBounds(70, 155, 103, 16);
-		btnFoglightSensor.setText("Foglight Sensor");
+		setLayout(new GridLayout(3, false));
+		 
+		 Label lblInitializeCar = new Label(this, SWT.SHADOW_IN | SWT.CENTER);
+		 lblInitializeCar.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 3, 1));
+		 lblInitializeCar.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
+		 lblInitializeCar.setText("Initialize a Car");
+		 
+		 btnAbsSensor = new Button(this, SWT.CHECK | SWT.CENTER);
+		 btnAbsSensor.setText("ABS Sensor");
+		 
+		 Label lblFuel = new Label(this, SWT.CENTER);
+		 lblFuel.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		 lblFuel.setAlignment(SWT.CENTER);
+		 lblFuel.setText("fuel \r\n[l]");
+		 
+		 fuel = new Text(this, SWT.BORDER | SWT.CENTER);
+		 GridData gd_fuel = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		 gd_fuel.widthHint = 41;
+		 fuel.setLayoutData(gd_fuel);
+		 fuel.setText("50");
+		 
+		 btnEspSensor = new Button(this, SWT.CHECK | SWT.CENTER);
+		 btnEspSensor.setText("ESP Sensor");
+		 
+		 Label lblSpeed = new Label(this, SWT.CENTER);
+		 lblSpeed.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		 lblSpeed.setText("max. speed \r\n[km/h]");
+		 lblSpeed.setAlignment(SWT.CENTER);
+		 
+		 maxspeed = new Text(this, SWT.BORDER | SWT.CENTER);
+		 GridData gd_maxspeed = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		 gd_maxspeed.widthHint = 42;
+		 maxspeed.setLayoutData(gd_maxspeed);
+		 maxspeed.setText("120");
+		 
+		 btnFoglightSensor = new Button(this, SWT.CHECK | SWT.CENTER);
+		 btnFoglightSensor.setText("Foglight Sensor");
+		 
+		 lblFuelConsumption = new Label(this, SWT.CENTER);
+		 lblFuelConsumption.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		 lblFuelConsumption.setText("consumption\r\n[l/100km]");
+		 
+		 consumption = new Text(this, SWT.BORDER | SWT.CENTER);
+		 GridData gd_consumption = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		 gd_consumption.widthHint = 42;
+		 consumption.setLayoutData(gd_consumption);
+		 consumption.setText("6.3");
+		 consumption.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		
 		 btnLightSensor = new Button(this, SWT.CHECK | SWT.CENTER);
-		 btnLightSensor.setBounds(70, 200, 93, 16);
-		btnLightSensor.setText("Light Sensor");
-		
-		Label lblInitializeCar = new Label(this, SWT.SHADOW_IN | SWT.CENTER);
-		lblInitializeCar.setBounds(73, 10, 283, 33);
-		lblInitializeCar.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
-		lblInitializeCar.setText("Initialize Car");
-		
-		lblFuelConsumption = new Label(this, SWT.CENTER);
-		lblFuelConsumption.setBounds(190, 138, 75, 33);
-		lblFuelConsumption.setText("fuel \r\nconsumption");
-		
-		consumption = new Text(this, SWT.BORDER | SWT.CENTER);
-		consumption.setBounds(280, 150, 76, 21);
-		consumption.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		 btnLightSensor.setText("Light Sensor");
 		
 		btnTrackfile = new Button(this, SWT.NONE);
-		btnTrackfile.setBounds(190, 200, 75, 25);
+		btnTrackfile.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 		btnTrackfile.setText("Trackfile");
-		
-		lblFileName = new Label(this, SWT.NONE);
-		lblFileName.setBounds(280, 201, 139, 24);
-		lblFileName.setAlignment(SWT.CENTER);
-		lblFileName.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
-		lblFileName.setText("none");
-
-		btnInitialize.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				float fmaxSpeed = 0, ffuel = 0, fuelConsumption = 0;
-				try{
-					fmaxSpeed = Float.parseFloat(maxspeed.getText());
-					ffuel = Float.parseFloat(fuel.getText());
-					fuelConsumption = Float.parseFloat(consumption.getText());
-				}catch(NumberFormatException e1){
-					JOptionPane.showConfirmDialog(null, "Fill textfields with numbers, please!", "Input Error", JOptionPane.OK_OPTION);
-					return;
-				}
-				
-				if(!trackFile.exists()){
-					JOptionPane.showConfirmDialog(null, "Choose a trackfile, please!","Input Error", JOptionPane.OK_OPTION);
-					return;
-				}
-				controller.initializeCar(btnAbsSensor.getSelection(), btnEspSensor.getSelection(), 
-				btnLightSensor.getSelection(), btnFoglightSensor.getSelection(), fmaxSpeed, ffuel, fuelConsumption, trackFile);
-				me.setVisible(false);
-				me.dispose();
-			}
-		});
 		
 		btnTrackfile.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -147,11 +118,50 @@ public class InitializeFrame extends Composite{
 		        chooser.setVisible(false); 
 			}
 		});
+		
+		lblFileName = new Label(this, SWT.NONE);
+		lblFileName.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblFileName.setAlignment(SWT.CENTER);
+		lblFileName.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
+		lblFileName.setText("none");
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		
+		trackFile = new File("E:\\Users\\kensan.Phenom-PC\\git\\PlayITS\\Car\\ttworkbench.play.widget.car.ui\\maps\\RoutenachArnimallee.txt");
+		lblFileName.setText(trackFile.getName());
+		Button btnInitialize_1 = new Button(this, SWT.NONE);
+		btnInitialize_1.setText("initialize");
+		
+				btnInitialize_1.addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+						float fmaxSpeed = 0, ffuel = 0, fuelConsumption = 0;
+						try{
+							fmaxSpeed = Float.parseFloat(maxspeed.getText());
+							ffuel = Float.parseFloat(fuel.getText());
+							fuelConsumption = Float.parseFloat(consumption.getText());
+						}catch(NumberFormatException e1){
+							JOptionPane.showConfirmDialog(null, "Fill textfields with numbers, please!", "Input Error", JOptionPane.OK_OPTION);
+							return;
+						}
+						
+						if(!trackFile.exists()){
+							JOptionPane.showConfirmDialog(null, "Choose a trackfile, please!","Input Error", JOptionPane.OK_OPTION);
+							return;
+						}
+						controller.initializeCar(btnAbsSensor.getSelection(), btnEspSensor.getSelection(), 
+						btnLightSensor.getSelection(), btnFoglightSensor.getSelection(), fmaxSpeed, ffuel, fuelConsumption, trackFile);
+						me.setVisible(false);
+						me.dispose();
+					}
+				});
+		new Label(this, SWT.NONE);
 	}
 
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
 	}
-
 }
