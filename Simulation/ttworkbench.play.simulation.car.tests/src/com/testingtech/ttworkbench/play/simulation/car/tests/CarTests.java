acdc752c5b3c2800a0e48458c0a14a82dbd6f495
Bogdan Stanca-Kaposta <stanca@testingtech.com>
2013-03-26 12:46:31 +0100

    car update tests

--- Simulation/ttworkbench.play.simulation.car.tests/src/com/testingtech/ttworkbench/play/simulation/car/tests/CarTests.java
+++ Simulation/ttworkbench.play.simulation.car.tests/src/com/testingtech/ttworkbench/play/simulation/car/tests/CarTests.java
@@ -3,11 +3,15 @@ package com.testingtech.ttworkbench.play.simulation.car.tests;
 import java.io.File;
 import java.net.MalformedURLException;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.testingtech.ttworkbench.play.generated.PROTO_API.carStatusType;
 import com.testingtech.ttworkbench.play.simulation.car.Car;
+import com.testingtech.ttworkbench.play.simulation.car.CarStatusTypeParser;
 
 public class CarTests {
 
@@ -25,7 +29,7 @@ public class CarTests {
 		car.disposeCar();
 		car = null;
 	}
-	
+
 	@Test
 	public void testCarParseTrack() throws MalformedURLException {
 		car.update();
@@ -36,6 +40,22 @@ public class CarTests {
 		car.setEngine(true);
 		car.setSpeed(50);
 		car.update();
+	}
+
+
+	@Test
+	public void testCarUpdate() throws MalformedURLException {
+		car.setEngine(true);
+		car.setSpeed(50);
+
+		int countUpdates = 200;
+		for (int i = 0; i < countUpdates; i++) {
+			car.update();
+			
+			// parse the car into a carStatusType message and make the rpc call
+			carStatusType request = CarStatusTypeParser.parseToStatusType(car);
+			Assert.assertFalse("Fail latitude is NaN", Float.isNaN(request.getGpsPos().getLatitude()));
+		}
 	}
 
 	@SuppressWarnings("deprecation")
